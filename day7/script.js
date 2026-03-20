2f037266a6747ed5e51ff05aaf60b573331a88e7
tyab07 <tyab.mujahid@gmail.com>
2026-03-20 11:32:47 +0500

    Day7 modified

--- day7/script.js
+++ day7/script.js
@@ -3,7 +3,7 @@ async function utility()
 {
     // fetch is used to get data from the server
 let p = await fetch("https://jsonplaceholder.typicode.com/post/1");
-// json is used to convert the data into json format
+// json is used to convert the data into json formatss
 let p2 = await p.json();
 console.log(p2);
 }
